4fe67cf38e6de305100d30e996af9dd7f6e15c8b
John Gockel <john.gockel@t-online.de>
2020-02-17 15:44:42 +0100

    add OpenAPI documentation

--- modules/spring-service-api/spring-service-api-rest/src/main/java/de/johngockel/service/api/rest/RestApiModuleConfiguration.java
+++ modules/spring-service-api/spring-service-api-rest/src/main/java/de/johngockel/service/api/rest/RestApiModuleConfiguration.java
@@ -1,5 +1,9 @@
 package de.johngockel.service.api.rest;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,5 +15,14 @@ public class RestApiModuleConfiguration implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
+    }
+
+    @Bean
+    public OpenAPI openAPI(@Value("${project.version}") String projectVersion) {
+        return new OpenAPI().info(
+                new Info().title("Spring Boot Multi Module Example API")
+                .version(projectVersion)
+                .description("Demo API")
+        );
     }
 }
